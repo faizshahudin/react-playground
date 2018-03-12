7b19df55d3643df40dfaa3d7899c520f4ea0835e
Faiz Shahudin <faizshahudin@gmail.com>
2018-03-12 11:33:39 +0800

    delete-useless-constructors

--- social-card/src/components/CardSection.js
+++ social-card/src/components/CardSection.js
@@ -5,10 +5,6 @@ import Card from './Card';
 import Response from './Response';
 
 class CardSection extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         let post = this.props.post;
         console.log(post);
--- social-card/src/components/Response.js
+++ social-card/src/components/Response.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import { Glyphicon } from 'react-bootstrap';
 
 class Response extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         let post = this.props.post;
 
